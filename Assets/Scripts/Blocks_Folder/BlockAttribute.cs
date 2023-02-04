9aef14312a3346ec8f84f284778206669a294954
WAI <why18520259443@gmail.com>
2023-02-04 14:46:04 +0800

    告一段落

--- Assets/Scripts/Blocks_Folder/BlockAttribute.cs
+++ Assets/Scripts/Blocks_Folder/BlockAttribute.cs
@@ -8,11 +8,12 @@ namespace Blocks_Folder
     [Serializable]
     public class BlockAttribute
     {
+        [FormerlySerializedAs("selectState")]
         [FormerlySerializedAs("curState")] 
-        [Tooltip("图块可选择状态（参照状态对照表）")] public int selectState;
+        [Tooltip("图块可选择状态（参照状态对照表）")] public int moveState;
         [Tooltip("图块类型")] public int type;
         [Tooltip("图块贴图")] public Image image;
-        [Tooltip("玩家踩上时的步数奖励")] public int stepReward;
+        [FormerlySerializedAs("stepReward")] [Tooltip("玩家踩上时的步数奖励")] public int stepAward;
         [Tooltip("图块所在方格位置")] public Vector2 locate;
 
     }
