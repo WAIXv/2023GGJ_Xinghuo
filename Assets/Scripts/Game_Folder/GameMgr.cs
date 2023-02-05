10027eddaf0c069ffb98d1840335a589bf4795cd
WAI <why18520259443@gmail.com>
2023-02-05 08:47:28 +0800

    差不多能打包了

--- Assets/Scripts/Game_Folder/GameMgr.cs
+++ Assets/Scripts/Game_Folder/GameMgr.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Blocks_Folder;
+using Game_Folder.UIScripts;
 using Map_Folder;
 using UnityEngine;
 using UnityEngine.Serialization;
@@ -13,24 +14,33 @@ public class GameMgr : MonoBehaviour
 
     public RectTransform basePoint;
     private float originY;
-    
+
+    [SerializeField] private int originStep;
     public int moveStep;
     public bool gameOn;
 
     public Vector2 startPoint;
 
     public float endY;
+
+    [SerializeField] private float midY;
+
+    [SerializeField] private float scrollSpeed;
     
     private int curLevel = 0;
 
-    public Animator treeAnim;
+    [SerializeField] private Animator treeAnim;
+
+    public GameObject leafObj;
 
     private void Start()
     {
         instance = this;
+        moveStep = originStep;
         originY = basePoint.anchoredPosition.y;
         EventCenter.GetInstance().AddEventListener(EventTypes.RootMove,OnRootMove);
         EventCenter.GetInstance().AddEventListener(EventTypes.LevelFinish,OnLevelFinish);
+        leafObj.SetActive(false);
     }
 
     private void Update()
@@ -64,14 +74,15 @@ public class GameMgr : MonoBehaviour
         var delta = Input.mouseScrollDelta * -50f;
 
         if(delta.y + basePoint.transform.localPosition.y < -490f || delta.y + basePoint.transform.localPosition.y > endY) return;
-        
-        basePoint.localPosition = new Vector3(basePoint.localPosition.x, basePoint.localPosition.y + delta.y,
-            basePoint.localPosition.z);
+
+        var localPosition = basePoint.localPosition;
+        localPosition = new Vector3(localPosition.x, localPosition.y + delta.y, localPosition.z);
+        basePoint.localPosition = localPosition;
     }
     
     public void OnLevelFinish()
     {
-        GameMgr.GetInstance().gameOn = false;
+        gameOn = false;
         switch (curLevel)
         {
             case 0:
@@ -83,14 +94,20 @@ public class GameMgr : MonoBehaviour
             case 2:
                 break;
         }
+    }
 
-        curLevel++;
+    public IEnumerator Level0Enter()
+    {
+        while (basePoint.anchoredPosition.y < midY)
+        {
+            SlideToMid();
+            yield return new WaitForSeconds(0.01f);
+        }
     }
 
     IEnumerator Level0Finish()
     {
-        var scrollSpeed = 20f;
-        Debug.Log(basePoint.anchoredPosition.y);
+        leafObj.SetActive(false);
         while (basePoint.anchoredPosition.y > originY)
         {
             basePoint.transform.Translate(Vector3.down * scrollSpeed);
@@ -100,15 +117,22 @@ public class GameMgr : MonoBehaviour
         PlayAnim(curLevel);
         yield return new WaitForSeconds(1f);
         ReloadMap(Resources.Load<TextAsset>("LevelMaptxt/level_1"));
-        gameOn = true;
         endY = originY + (MapCreator.GetInstance()._mapMatrix.Length - 1) * 80f;
-
+        moveStep = originStep;
+        curLevel++;
+        while (basePoint.anchoredPosition.y < midY)
+        {
+            SlideToMid();
+            yield return new WaitForSeconds(0.01f);
+        }
+        leafObj.GetComponent<LeafScript>().textComp.text = moveStep.ToString();
+        leafObj.SetActive(true);
+        gameOn = true;
     }
 
     IEnumerator Level1Finish()
     {
-        var scrollSpeed = 20f;
-        Debug.Log(basePoint.anchoredPosition.y);
+        leafObj.SetActive(false);
         while (basePoint.anchoredPosition.y > originY)
         {
             basePoint.transform.Translate(Vector3.down * scrollSpeed);
@@ -118,13 +142,27 @@ public class GameMgr : MonoBehaviour
         PlayAnim(curLevel);
         yield return new WaitForSeconds(1f);
         ReloadMap(Resources.Load<TextAsset>("LevelMaptxt/level_2"));
-        gameOn = true;
         endY = originY +  (MapCreator.GetInstance()._mapMatrix.Length - 1) * 80f;
+        moveStep = originStep;
+        curLevel++;
+        while (basePoint.anchoredPosition.y < midY)
+        {
+            SlideToMid();
+            yield return new WaitForSeconds(0.01f);
+        }
+        leafObj.GetComponent<LeafScript>().textComp.text = moveStep.ToString();
+        leafObj.SetActive(true);
+        gameOn = true;
     }
 
     IEnumerator Level2Finish()
     {
         yield return null;
+    }
+
+    void SlideToMid()
+    {
+        basePoint.transform.Translate(Vector3.up * scrollSpeed);
     }
 
     void PlayAnim(int level)
